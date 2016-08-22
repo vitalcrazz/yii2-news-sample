651dedff5963918a9b42f98c6ad3aa705b3611da
vitalcrazz <zojl@yandex.ru>
2016-08-22 10:15:40 +0300

    demo data migration

---
+++ console/migrations/m160822_062747_insert_demo_data.php
@@ -0,0 +1,44 @@
+<?php
+
+use yii\db\Migration;
+
+class m160822_062747_insert_demo_data extends Migration
+{
+    public function up()
+    {
+        $this->insert('{{%user}}', [
+            'username' => 'admin',
+            'auth_key' => 'wczBMhNmaoyPldfXbS0-96Jylc-1KnZK',
+            'password_hash' => '$2y$13$LTHj17vLtiSVNmhETi41HO.uvCFHnfJ8Bte1Y3wwRPVF5TOJ3amf6',
+            'email' => 'zojl@yandex.ru',
+        ]);
+
+        $this->batchInsert('{{%subjects}}', ['name'], [
+            ['Бизнес'],
+            ['Культура'],
+            ['Спорт'],
+            ['Наука и техника'],
+            ['Другие новости'],
+        ]);
+    }
+
+    public function down()
+    {
+        $this->delete('{{%user}}', ['username' => 'admin']);
+
+        $this->truncateTable('{{%subjects}}');
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
